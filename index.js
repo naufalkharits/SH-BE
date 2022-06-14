d83b94754c7b44e14b3f8e1d10994892c1358f29
WawanC <kurniawancristianto@gmail.com>
2022-06-14 14:10:10 +0700

    add router

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const productRoutes = require("./routes/product");
 
 const app = express();
+
+app.use("/product", productRoutes);
 
 app.listen(8000, async () => {
   console.log("Listening on 8000");
