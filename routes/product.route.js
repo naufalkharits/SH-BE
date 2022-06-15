da56379bf4b3095ad82fe52eea2cc19532a16a4b
WawanC <kurniawancristianto@gmail.com>
2022-06-15 12:34:38 +0700

    fix routing

--- routes/product.route.js
+++ routes/product.route.js
@@ -1,3 +1,7 @@
 const router = require("express").Router();
+const controller = require("../controllers/product.controller");
+
+router.get("/", controller.getProducts);
+router.get("/:id", controller.getProduct);
 
 module.exports = router;
