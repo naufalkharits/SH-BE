ee7bf35f8c33021efa3b0f9f4bba7c8612ecc09d
Muhammad Zaky <mhmdzakyy10@gmail.com>
2022-06-15 21:24:23 +0700

    add features update and delete product

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,18 +1,14 @@
-const Product = require("../models").Product;
-const Category = require("../models").Category;
+const { Product } = require("../models");
+const { Category } = require("../models");
 
 module.exports = {
   getProducts: (req, res) => {
     Product.findAll()
       .then((result) => {
         if (result.length > 0) {
-          res
-            .status(200)
-            .json({ message: "SUKSES Get All PRODUCT", data: result });
+          res.status(200).json({ message: "SUKSES Get All PRODUCT", data: result });
         } else {
-          res
-            .status(404)
-            .json({ message: "PRODUCT Tidak DiTemukan!", data: result });
+          res.status(404).json({ message: "PRODUCT Tidak DiTemukan!", data: result });
         }
       })
       .catch((err) => {
@@ -33,8 +29,7 @@ module.exports = {
           res.status(200).json({ message: "SUKSES Get PRODUCT By Id", result });
         } else {
           res.status(404).json({
-            message:
-              " PRODUCT dengan ID " + req.params.id + " Tidak DiTemukan!",
+            message: " PRODUCT dengan ID " + req.params.id + " Tidak DiTemukan!",
             result,
           });
         }
@@ -44,6 +39,64 @@ module.exports = {
           message: "GAGAL Get PRODUCT By Id",
           err: err.message,
         });
+      });
+  },
+  updateProduct: (req, res) => {
+    const { name, price, category, description } = req.body;
+    console.log(req.body);
+    if (!Number.isInteger(+req.params.id)) {
+      return res.status(400).json({ message: "ID Must Be Integer" });
+    }
+    Category.findOne({
+      where: {
+        name: category,
+      },
+    }).then((result) => {
+      if (!result) {
+        return res.status(400).json({ message: "Category Not Found" });
+      }
+      Product.update(
+        {
+          name: name,
+          price: price,
+          category_id: result.id,
+          description: description,
+        },
+        {
+          where: {
+            id: +req.params.id,
+          },
+          returning: true,
+        }
+      )
+        .then((result) => {
+          if (result[0] === 0) {
+            res.status(400).json({ message: "Data Not Found!" });
+          } else {
+            res.status(200).json({ message: "Product Updated", data: result[1] });
+          }
+        })
+        .catch((err) => {
+          res.status(400).json({ message: "Error Updating User", err: err.message });
+        });
+    });
+  },
+
+  deleteProduct: (req, res) => {
+    if (!Number.isInteger(+req.params.id)) {
+      return res.status(400).json({ message: "ID Must Be Integer" });
+    }
+    Product.destroy({ where: { id: req.params.id } })
+      .then((result) => {
+        console.log(result);
+        if (result === 0) {
+          res.status(400).json({ message: "Data Not Found!" });
+        } else {
+          res.status(200).json({ message: "Product Deleted" });
+        }
+      })
+      .catch((err) => {
+        res.status(500).json({ message: "Error Deleting Product", err: err.message });
       });
   },
 };
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = require("./routes/index.route");
 const app = express();
 
+app.use(express.json());
 app.use(router);
 
 app.listen(8000, async () => {
