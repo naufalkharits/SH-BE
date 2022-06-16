edc00759a3b1d20f5d908e1f4c925cfd7c13f049
WawanC <kurniawancristianto@gmail.com>
2022-06-16 18:09:00 +0700

    add cors

--- index.js
+++ index.js
@@ -2,11 +2,19 @@ const express = require("express");
 const router = require("./routes/index.route");
 const app = express();
 const path = require("path");
+const cors = require("cors");
+const fs = require("fs");
 
+if (!fs.existsSync(path.join(__dirname, "public", "images"))) {
+  fs.mkdirSync(path.join(__dirname, "public", "images"), {
+    recursive: true,
+  });
+}
+
+app.use(cors());
+app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
-app.use(express.json());
 
-app.use(express.json());
 app.use(router);
 
 app.listen(process.env.PORT || 8000, async () => {
