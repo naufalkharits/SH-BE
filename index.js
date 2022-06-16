fc8812bc3d38176c433f01d31ec4b06ad22dfe9e
unknown <adelia.rahmawati19@mhs.ubharajaya.ac.id>
2022-06-16 19:55:59 +0700

    swaggerJson

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ const router = require("./routes/index.route");
 const app = express();
 const path = require("path");
 const cors = require("cors");
+const swaggerJSON = require("./swagger.json");
+const swaggerUI = require("swagger-ui-express");
 
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
+app.use("./docs", swaggerUI.serve, swaggerUI.setup(swaggerJSON));
 
 app.use(router);
 
