809454f4d04e45e4d92293112ffacba531a7a84d
WawanC <kurniawancristianto@gmail.com>
2022-06-16 16:31:33 +0700

    change category seeder

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const express = require("express");
 const router = require("./routes/index.route");
 const app = express();
 const path = require("path");
+const cors = require("cors");
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
+app.use(cors());
 
-app.use(express.json());
 app.use(router);
 
 app.listen(process.env.PORT || 8000, async () => {
