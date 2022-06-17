a16bb6722324acf55f220d9260b88339e465f289
WawanC <kurniawancristianto@gmail.com>
2022-06-17 18:57:54 +0700

    update private key

--- utils/firebase-storage.js
+++ utils/firebase-storage.js
@@ -1,11 +1,10 @@
 const admin = require("firebase-admin");
-// const serviceAccount = require("../serviceAccountKey.json");
 
 const setup = () => {
   admin.initializeApp({
     credential: admin.credential.cert({
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
       projectId: process.env.FIREBASE_PROJECT_ID,
     }),
     storageBucket: process.env.FIREBASE_BUCKET_URL,
