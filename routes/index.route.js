a5f41d219d971fa889d84a6744c7ebbad14d240b
unknown <adelia.rahmawati19@mhs.ubharajaya.ac.id>
2022-06-20 15:43:43 +0700

    add login

--- routes/index.route.js
+++ routes/index.route.js
@@ -1,8 +1,12 @@
 const router = require("express").Router();
 const productRoutes = require("./product.route");
+const { auth } = require("../controllers/auth");
 
 router.use("/product", productRoutes);
 
+router.post("/login", login);
+
 router.use("/update-product", productRoutes);
 router.use("/delete-product", productRoutes);
+
 module.exports = router;
