b2eea27ff15e394b721a1480487511a8cba4e2e1
Muhammad Zaky <mhmdzakyy10@gmail.com>
2022-06-21 21:57:21 +0700

    add testing register

--- tests/product.spec.js
+++ tests/product.spec.js
@@ -188,3 +188,26 @@ describe("Delete Product", () => {
       .expect(500);
   });
 });
+
+describe("Register", () => {
+  test("200 Success", async () => {
+    await request(app).post("/auth/register").send({ email: "test321@gmail.com", password: "123456" }).expect(200);
+  });
+
+  test("409 Email already exists", async () => {
+    await request(app).post("/auth/register").send({ email: "test321@gmail.com", password: "123456" }).expect(409);
+  });
+
+  test("400 Invalid Email", async () => {
+    await request(app).post("/auth/register").send({ email: "inibukanemail", password: "123456" }).expect(400);
+  });
+
+  test("500 Failed Create New User", async () => {
+    User.create = jest.fn().mockImplementationOnce(() => {
+      throw new Error();
+    });
+    await request(app).post("/auth/register").send({ email: "test3210@gmail.com", password: "123456" }).expect(500);
+  });
+});
+
+module.exports = { testUser };
