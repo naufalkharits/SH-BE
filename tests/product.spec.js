8c968d8c5771b141a31298a4a61f173f028a6944
WawanC <kurniawancristianto@gmail.com>
2022-06-21 18:40:56 +0700

    update testing

--- tests/product.spec.js
+++ tests/product.spec.js
@@ -13,85 +13,92 @@ const newProductData = {
   pictures: path.join(__dirname, "resources", "product.png"),
 };
 
+let testUser, testProduct;
+
+beforeAll(async () => {
+  testUser = await User.create({
+    email: "test@gmail.com",
+    password: "test123",
+  });
+  testProduct = await Product.create({
+    name: "New Test Product",
+    price: 50000,
+    category_id: 3,
+    description: "This is new test product",
+    seller_id: testUser.id,
+  });
+});
+
 afterAll(async () => {
+  await User.destroy({ where: {} });
   await Product.destroy({ where: {} });
   server.close();
 });
+
+describe("Get Products", () => {
+  test("200 Success", async () => {
+    await request(app).get("/product").expect(200);
+  });
+});
+
 describe("Get Product", () => {
-  test("Get Product", async () => {
-    const { statusCode, error } = await request(app).get("/product");
-    expect(statusCode).toEqual(200);
+  test("200 Success", async () => {
+    await request(app)
+      .get("/product/" + testProduct.id)
+      .expect(200);
+  });
+});
+
+describe("Create Product", () => {
+  test("200 Success", async () => {
+    await request(app)
+      .post("/product")
+      .field("name", newProductData.name)
+      .field("price", newProductData.price)
+      .field("category", newProductData.category)
+      .field("description", newProductData.description)
+      .attach("pictures", newProductData.pictures)
+      .expect(200);
   });
 
-  test("Get Product by ID", async () => {
-    const newUser = await User.create({
-      email: "a@gmail.com",
-      password: "test123",
-    });
-    const newProduct = await Product.create({
-      name: "New Test Product",
-      price: 50000,
-      category_id: 3,
-      description: "This is new test product",
-      seller_id: newUser.id,
-    });
-    console.log(newProduct);
-    const { body, statusCode, error } = await request(app).get(
-      "/product/" + newProduct.id
-    );
-    expect(statusCode).toEqual(200);
+  test("400 Validation Failed", async () => {
+    await request(app).post("/product").expect(400);
   });
-}),
-  describe("Create Product", () => {
-    test("200 Success", async () => {
-      await request(app)
-        .post("/product")
-        .field("name", newProductData.name)
-        .field("price", newProductData.price)
-        .field("category", newProductData.category)
-        .field("description", newProductData.description)
-        .attach("pictures", newProductData.pictures)
-        .expect(200);
-    });
 
-    test("400 Validation Failed", async () => {
-      await request(app).post("/product").expect(400);
-    });
-
-    test("400 Picture Validation Failed", async () => {
-      await request(app)
-        .post("/product")
-        .field("name", newProductData.name)
-        .field("price", newProductData.price)
-        .field("category", newProductData.category)
-        .field("description", newProductData.description)
-        .attach("pictures", path.join(__dirname, "resources", "product.txt"))
-        .expect(400);
-    });
-
-    test("400 Invalid Category", async () => {
-      await request(app)
-        .post("/product")
-        .field("name", newProductData.name)
-        .field("price", newProductData.price)
-        .field("category", "invalid")
-        .field("description", newProductData.description)
-        .attach("pictures", newProductData.pictures)
-        .expect(400);
-    });
-
-    test("500 System Error", async () => {
-      Product.create = jest.fn().mockImplementationOnce(() => {
-        throw new Error();
-      });
-
-      await request(app)
-        .post("/product")
-        .field("name", newProductData.name)
-        .field("price", newProductData.price)
-        .field("category", newProductData.category)
-        .field("description", newProductData.description)
-        .attach("pictures", newProductData.pictures)
-        .expect(500);
-    });
+  test("400 Picture Validation Failed", async () => {
+    await request(app)
+      .post("/product")
+      .field("name", newProductData.name)
+      .field("price", newProductData.price)
+      .field("category", newProductData.category)
+      .field("description", newProductData.description)
+      .attach("pictures", path.join(__dirname, "resources", "product.txt"))
+      .expect(400);
   });
+
+  test("400 Invalid Category", async () => {
+    await request(app)
+      .post("/product")
+      .field("name", newProductData.name)
+      .field("price", newProductData.price)
+      .field("category", "invalid")
+      .field("description", newProductData.description)
+      .attach("pictures", newProductData.pictures)
+      .expect(400);
+  });
+
+  test("500 System Error", async () => {
+    Product.create = jest.fn().mockImplementationOnce(() => {
+      throw new Error();
+    });
+
+    await request(app)
+      .post("/product")
+      .field("name", newProductData.name)
+      .field("price", newProductData.price)
+      .field("category", newProductData.category)
+      .field("description", newProductData.description)
+      .attach("pictures", newProductData.pictures)
+      .expect(500);
+  });
+});
