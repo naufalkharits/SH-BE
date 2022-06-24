704e48336bcbc8d64cc4977e41144d9284c9b24f
WawanC <kurniawancristianto@gmail.com>
2022-06-24 20:51:33 +0700

    update cors

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
     "Access-Control-Allow-Methods",
-    "GET, POST, PUT, PATCH, DELETE"
+    "GET, POST, PUT, PATCH, DELETE, OPTIONS"
   );
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
