a5b2cb4eedcda7009ecc2b915da772a66c9b9e8d
WawanC <kurniawancristianto@gmail.com>
2022-06-27 12:11:10 +0700

    fix get biodata

--- routes/index.route.js
+++ routes/index.route.js
@@ -2,11 +2,11 @@ const router = require("express").Router();
 const productRoutes = require("./product.route");
 const authRoutes = require("./auth.route");
 const categoryRoutes = require("./category.route");
-const userbiodataRoutes = require("./userbiodata.route");
+const biodataRoutes = require("./userbiodata.route");
 
 router.use("/product", productRoutes);
 router.use("/auth", authRoutes);
 router.use("/category", categoryRoutes);
-router.use("/userbiodata", userbiodataRoutes);
+router.use("/biodata", biodataRoutes);
 
 module.exports = router;
--- routes/userbiodata.route.js
+++ routes/userbiodata.route.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const controller = require("../controllers/userbiodata.controller");
 
-router.get("/:id", controller.getuserbiodata);
+router.get("/:id", controller.getBiodata);
 
 module.exports = router;
