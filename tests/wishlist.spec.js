ad51a486feb2f8fbdced4382e346631c7cee726c
unknown <adelia.rahmawati19@mhs.ubharajaya.ac.id>
2022-06-30 20:39:58 +0700

    add testing delete wishlist

--- tests/wishlist.spec.js
+++ tests/wishlist.spec.js
@@ -1,6 +1,7 @@
 const request = require("supertest");
 const { app, server } = require("../index");
 const { Wishlist, Product, User } = require("../models");
+const { bcrypt } = require("bcrypt");
 
 let testUser, testProduct;
 
@@ -46,4 +47,49 @@ describe("Create Wishlist", () => {
   test("500 System Error", () => {});
 });
 
-describe("Delete Wishlist", () => {});
+describe("Delete Wishlist", () => {
+  test("200 Success", async () => {
+    await request(app)
+      .delete("/wishlist/")
+      .send({
+        product_id: testProduct.id,
+        user_id: testUser.id,
+      })
+      .expect(200);
+  });
+
+  test("400 Validation Failed", async () => {
+    await request(app)
+      .delete("/product/")
+      .send({
+        product_id: testProduct.id,
+        user_id: testUser.id,
+      })
+      .expect(400);
+  });
+
+  test("404 Product Not Found", async () => {
+    await request(app)
+      .delete("/wishlist/")
+      .send({
+        product_id: "testProduct",
+        user_id: "2",
+      })
+      .expect(404);
+  });
+
+  test("500 System Error", async () => {
+    const originalFn = Product.destroy;
+    Product.destroy = jest.fn().mockImplementationOnce(() => {
+      throw new Error();
+    });
+    await request(app)
+      .delete("/product/" + testProduct.id)
+      .send({
+        product_id: testProduct.id,
+        user_id: testUser.id,
+      })
+      .expect(500);
+    Product.destroy = originalFn;
+  });
+});
