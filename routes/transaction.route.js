359d356969f88a0909881380e8b29787f26caa56
Muhammad Zaky <mhmdzakyy10@gmail.com>
2022-07-06 20:12:13 +0700

    Testing and Documentation Update Delete Transaction

--- routes/transaction.route.js
+++ routes/transaction.route.js
@@ -3,6 +3,6 @@ const controller = require("../controllers/transaction.controller");
 const checkAuth = require("../middlewares/check-auth");
 
 router.post("/:productId", checkAuth, controller.createTransaction);
-router.put("/:id", checkAuth, controller.updateTransaction);
+router.put("/:id", controller.updateTransaction);
 router.delete("/:id", controller.deleteTransaction);
 module.exports = router;
