d26d6f2ee3c6f4fabbfc175e15fae3875717f3b4
Muhammad Zaky <mhmdzakyy10@gmail.com>
2022-07-11 21:53:49 +0700

    add API Update Notification

--- controllers/notification.controller.js
+++ controllers/notification.controller.js
@@ -2,6 +2,48 @@ const { Notification } = require("../models");
 
 module.exports = {
   getNotifications: (req, res) => {},
-  updateNotification: (req, res) => {},
+  updateNotification: async (req, res) => {
+    if (!Number.isInteger(+req.params.id)) {
+      return res.status(400).json({
+        type: "VALIDATION_FAILED",
+        message: "Invalid notification id",
+      });
+    }
+
+    if (req.body.read && typeof req.body.read !== "boolean") {
+      return res.status(400).json({
+        type: "VALIDATION_FAILED",
+        message: "Invalid read value",
+      });
+    }
+
+    const { read } = req.body;
+
+    try {
+      await Notification.update(
+        { read: read },
+        {
+          where: { id: req.params.id },
+        }
+      );
+
+      const notification = await Notification.findOne({
+        where: { id: req.params.id },
+      });
+
+      if (!notification) {
+        return res.status(404).json({
+          type: "NOT_FOUND",
+          message: "Notification not found",
+        });
+      }
+      res.status(200).json({ notification });
+    } catch (err) {
+      return res.status(500).json({
+        type: "SERVER_ERROR",
+        message: "Something went wrong",
+      });
+    }
+  },
   deleteNotification: (req, res) => {},
 };
