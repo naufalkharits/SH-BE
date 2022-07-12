fa19cb0a753408d1caae654c68b57d6c31c5a5ec
unknown <adelia.rahmawati19@mhs.ubharajaya.ac.id>
2022-07-12 19:13:10 +0700

    Get Notification

--- tests/notification.spec.js
+++ tests/notification.spec.js
@@ -8,8 +8,59 @@ afterAll(async () => {
   server.close();
 });
 
-describe("Get Notifications", () => {});
+describe("Get Notifications", () => {
+  test("200 Success", async () => {
+    await request(app)
+      .get("/notification")
+      .set("Authorization", testUserAccessToken)
+      .expect(200);
+  });
+  test("500 System Error", async () => {
+    const originalFn = Notification.findAll;
+    Notification.findAll = jest.fn().mockImplementationOnce(() => {
+      throw new Error();
+    });
+    await request(app)
+      .get("/notification")
+      .set("Authorization", testUserAccessToken)
+      .expect(500);
+    Notification.findAll = originalFn;
+  });
+});
 
 describe("Update Notification", () => {});
 
-describe("Delete Notification", () => {});
+describe("Delete Notification", () => {
+  test("200 Success", async () => {
+    await request(app)
+      .delete("/notification/" + testNotification.id)
+      .set("Authorization", testUserAccessToken)
+      .expect(200);
+  });
+
+  test("400 Validation Failed", async () => {
+    await request(app)
+      .delete("/notification/abc")
+      .set("Authorization", testUserAccessToken)
+      .expect(400);
+  });
+
+  test("404 Notification Not Found", async () => {
+    await request(app)
+      .delete("/notification/0")
+      .set("Authorization", testUserAccessToken)
+      .expect(404);
+  });
+
+  test("500 System Error", async () => {
+    const originalFn = Notification.destroy;
+    Notification.destroy = jest.fn().mockImplementationOnce(() => {
+      throw new Error();
+    });
+    await request(app)
+      .delete("/notification/" + testNotification.id)
+      .set("Authorization", testUserAccessToken)
+      .expect(500);
+    Notification.destroy = originalFn;
+  });
+});
