3c741c6e453f1933ef2104ba9d6997479c9a54ee
Muhammad Zaky <mhmdzakyy10@gmail.com>
2022-07-19 19:07:32 +0700

    initial commit getChat

--- controllers/chat.controller.js
+++ controllers/chat.controller.js
@@ -1,6 +1,42 @@
 const { Chat, ChatMessage } = require("../models");
+const chatmessage = require("../models/chatmessage");
 
 module.exports = {
   getChats: (req, res) => {},
-  getChat: (req, res) => {},
+  getChat: async (req, res) => {
+    if (!req.params || !req.params.id || !Number.isInteger(+req.params.id)) {
+      return res.status(400).json({
+        type: "VALIDATION_FAILED",
+        message: "Valid User Biodata ID is required",
+      });
+    }
+    try {
+      const chat = await Chat.findOne({
+        where: {
+          id: req.params.id,
+        },
+      });
+      const ChatMessage = await ChatMessage.findOne({
+        where: {
+          chat_id: req.params.id,
+        },
+      });
+
+      if (!chat && !ChatMessage) {
+        return res.status(404).json({
+          type: "NOT_FOUND",
+          message: "Chat not found",
+        });
+      }
+
+      res.status(200).json({
+        chatmessage,
+      });
+    } catch (error) {
+      res.status(500).json({
+        type: "SYSTEM_ERROR",
+        message: "Something wrong with server",
+      });
+    }
+  },
 };
