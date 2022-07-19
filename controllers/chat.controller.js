ea882f99e3763415c8fd10ea44f2ec2654771bdd
unknown <adelia.rahmawati19@mhs.ubharajaya.ac.id>
2022-07-19 20:51:36 +0700

    Get Chats

--- controllers/chat.controller.js
+++ controllers/chat.controller.js
@@ -1,6 +1,16 @@
 const { Chat, ChatMessage } = require("../models");
 
 module.exports = {
-  getChats: (req, res) => {},
+  getChats: async (req, res) => {
+    try {
+      const chats = await Chat.findAll();
+      res.status(200).json({ chats });
+    } catch (error) {
+      res.status(500).json({
+        type: "SYSTEM_ERROR",
+        message: "Something wrong with server",
+      });
+    }
+  },
   getChat: (req, res) => {},
 };
