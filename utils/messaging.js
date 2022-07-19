c0bc5c7d012270c262d0d982db78d4f65eb9cc77
WawanC <kurniawancristianto@gmail.com>
2022-07-19 19:49:22 +0700

    add fcm token socket listener

--- utils/messaging.js
+++ utils/messaging.js
@@ -1,20 +1,20 @@
 const admin = require("firebase-admin");
 const { User } = require("../models");
 
-const sendNewPushNotification = async (userId) => {
+const sendNewPushNotification = async (fcmToken) => {
   try {
-    const user = await User.findOne({
-      where: { id: userId },
-    });
+    // const user = await User.findOne({
+    //   where: { id: userId },
+    // });
 
-    if (!user || !user.fcm_token) return;
+    // if (!user || !user.fcm_token) return;
 
     const response = await admin.messaging().send({
       notification: {
         title: "Hello World",
         body: "Hello from SecondHand NodeJS Server",
       },
-      token: user.fcm_token,
+      token: fcmToken,
     });
 
     console.log("Send Push Notification Done :", response);
