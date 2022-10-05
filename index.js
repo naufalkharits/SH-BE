73c03903f91610b366fab8b7e13eac371d332b41
Naufal <80172679+naufalkharits@users.noreply.github.com>
2022-10-05 11:08:57 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,39 +1,39 @@
-const express = require("express");
-const router = require("./routes/index.route");
-const app = express();
-const path = require("path");
-const swaggerJSON = require("./swagger.json");
-const swaggerUI = require("swagger-ui-express");
-const firebase = require("./utils/firebase");
-const socket = require("./utils/socket");
-require("./utils/passport-jwt");
+const express = require("express")
+const router = require("./routes/index.route")
+const app = express()
+const path = require("path")
+const swaggerJSON = require("./swagger.json")
+const swaggerUI = require("swagger-ui-express")
+const firebase = require("./utils/firebase")
+const socket = require("./utils/socket")
+require("./utils/passport-jwt")
 
-firebase.setup();
+firebase.setup()
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "*")
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, PUT, PATCH, DELETE, OPTIONS"
-  );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  )
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization")
   if (req.method == "OPTIONS") {
-    return res.sendStatus(200);
+    return res.sendStatus(200)
   } else {
-    next();
+    next()
   }
-});
+})
 
-app.use(express.json());
-app.use(express.static(path.join(__dirname, "public")));
-app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerJSON));
+app.use(express.json())
+app.use(express.static(path.join(__dirname, "public")))
+app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerJSON))
 
-app.use(router);
+app.use(router)
 
-const server = app.listen(process.env.PORT || 8000, async () => {
-  console.log(`Listening on ${process.env.PORT || 8000}`);
-});
+const server = app.listen(7610, async () => {
+  console.log("Listening on 7610")
+})
 
-socket.init(server);
+socket.init(server)
 
-module.exports = { app, server };
+module.exports = { app, server }
