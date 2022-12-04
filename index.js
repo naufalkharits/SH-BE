71ee5ed25418cf9aeff19e2ae068c316afa5d2e6
Naufal <80172679+naufalkharits@users.noreply.github.com>
2022-12-04 09:27:08 +0700

    Redeploy

--- index.js
+++ index.js
@@ -12,10 +12,7 @@ firebase.setup()
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*")
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, PATCH, DELETE, OPTIONS"
-  )
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization")
   if (req.method == "OPTIONS") {
     return res.sendStatus(200)
