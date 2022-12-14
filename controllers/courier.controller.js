b29c81600cef8bf1b093230a083b2b8897395659
Naufal <80172679+naufalkharits@users.noreply.github.com>
2022-12-15 06:28:06 +0700

    fix: separate config instead of axios.defaults to prevent future problems

--- controllers/courier.controller.js
+++ controllers/courier.controller.js
@@ -1,37 +1,52 @@
 const axios = require("axios")
 
-axios.defaults.baseURL = `${process.env.COURIER_API_URL}`
-axios.defaults.headers.common["key"] = `${process.env.COURIER_API_KEY}`
-axios.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded"
-
 module.exports = {
   getProvinces: async (req, res) => {
     try {
-      const response = await axios.get("/province")
-      res.json(response.data)
+      const response = await axios.get(`${process.env.COURIER_API_URL}/province`, {
+        headers: {
+          key: `${process.env.COURIER_API_KEY}`,
+        },
+      })
+      res.status(200).json(response.data)
     } catch (error) {
-      res.send(error)
+      res.status(500).json({ type: "SYSTEM_ERROR", message: "Something wrong with server" })
     }
   },
   getCities: async (req, res) => {
     try {
-      const response = await axios.get(`/city?province=${req.body.provinceId}`)
-      res.json(response.data)
+      const response = await axios.get(
+        `${process.env.COURIER_API_URL}/city?province=${req.body.provinceId}`,
+        {
+          headers: {
+            key: `${process.env.COURIER_API_KEY}`,
+          },
+        }
+      )
+      res.status(200).json(response.data)
     } catch (error) {
       res.send(error)
     }
   },
   getCosts: async (req, res) => {
     try {
-      const response = await axios.post("/cost", {
-        origin: req.body.origin,
-        destination: req.body.destination,
-        weight: req.body.weight,
-        courier: req.body.courier,
-      })
+      const response = await axios.post(
+        `${process.env.COURIER_API_URL}/cost`,
+        {
+          origin: req.body.origin,
+          destination: req.body.destination,
+          weight: req.body.weight,
+          courier: req.body.courier,
+        },
+        {
+          headers: {
+            key: `${process.env.COURIER_API_KEY}`,
+          },
+        }
+      )
       res.json(response.data)
     } catch (error) {
-      res.send(error)
+      res.status(500).json({ type: "SYSTEM_ERROR", message: "Something wrong with server" })
     }
   },
 }
