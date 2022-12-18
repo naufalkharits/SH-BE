6f911faa3c7492ca22dc55232bdcba856a779a21
Naufal <80172679+naufalkharits@users.noreply.github.com>
2022-12-19 00:04:27 +0700

    refactor: delete webhookInvoice controller

--- routes/payment.route.js
+++ routes/payment.route.js
@@ -1,7 +1,6 @@
 const router = require("express").Router()
 const paymentController = require("../controllers/payment.controller")
 
-router.post("/invoice/webhook", paymentController.webhookInvoice)
 router.post("/invoice", paymentController.createInvoice)
 
 module.exports = router
