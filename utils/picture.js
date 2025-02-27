ff83345b38844c71e9784c834ecf0138d4b58471
Naufal <80172679+naufalkharits@users.noreply.github.com>
2025-02-28 04:12:10 +0700

    supabase

--- utils/picture.js
+++ utils/picture.js
@@ -1,169 +1,191 @@
-// const Picture = require("../models").Picture;
-// const UserBiodata = require("../models").UserBiodata;
-// const { v4 } = require("uuid");
+const Picture = require("../models").Picture;
+const UserBiodata = require("../models").UserBiodata;
+const { v4 } = require("uuid");
 // const admin = require("firebase-admin");
+const { createClient } = require('@supabase/supabase-js');
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
 
-// const validatePicture = (picture) => {
-//   let acceptedMimetypes = ["image/png", "image/jpg", "image/jpeg"];
+const validatePicture = (picture) => {
+  let acceptedMimetypes = ["image/png", "image/jpg", "image/jpeg"];
 
-//   if (acceptedMimetypes.indexOf(picture.mimetype) < 0) {
-//     throw new Error("Valid picture format is required");
-//   }
+  if (acceptedMimetypes.indexOf(picture.mimetype) < 0) {
+    throw new Error("Valid picture format is required");
+  }
 
-//   if (picture.size > 5 * 1000 * 1000) {
-//     throw new Error("Picture size cannot be larger than 5 MB");
-//   }
-// };
+  if (picture.size > 5 * 1000 * 1000) {
+    throw new Error("Picture size cannot be larger than 5 MB");
+  }
+};
 
-// const uploadProductImages = async (images, productId) => {
-//   if (!images || images.length < 1) return;
+const uploadProductImages = async (images, productId) => {
+  if (!images || images.length < 1) return;
 
-//   try {
-//     // Upload updated pictures
-//     for (const image of images) {
-//       const newPictureName = v4();
+  try {
+    // Upload updated pictures
+    for (const image of images) {
+      const newPictureName = v4();
 
-//       const imageExt = image.mimetype.replace("image/", "");
-//       const imageName = `${newPictureName}.${imageExt}`;
-//       const imagePath = `images/${imageName}`;
+      const imageExt = image.mimetype.replace("image/", "");
+      const imageName = `${newPictureName}.${imageExt}`;
+      const imagePath = `images/${imageName}`;
 
-//       // Upload picture
-//       await admin.storage().bucket().file(imagePath).save(image.buffer);
+      // Upload picture
+      // await admin.storage().bucket().file(imagePath).save(image.buffer);
 
-//       await admin.storage().bucket().file(imagePath).makePublic();
+      // await admin.storage().bucket().file(imagePath).makePublic();
 
-//       const publicUrl = admin.storage().bucket().file(imagePath).publicUrl();
+      // const publicUrl = admin.storage().bucket().file(imagePath).publicUrl();
 
-//       // Add picture to DB
-//       await Picture.create({
-//         product_id: productId,
-//         name: imageName,
-//         url: publicUrl,
-//       });
-//     }
-//   } catch (error) {
-//     throw error;
-//   }
-// };
+      const { data, error } = await supabase.storage.from('secondhand').upload(imagePath, file);
+      console.log(data)
 
-// const updateProductImages = async (images, productId) => {
-//   if (!images || images.length < 1) return;
+      const { publicURL, error: urlError } = supabase.storage.from('secondhand').getPublicUrl(imagePath);
+      console.log(publicURL)
 
-//   try {
-//     await deleteProductImages(productId);
 
-//     await uploadProductImages(images, productId);
-//   } catch (error) {
-//     throw error;
-//   }
-// };
 
-// const deleteProductImages = async (productId) => {
-//   try {
-//     // Get existing pictures
-//     const pictures = await Picture.findAll({
-//       where: {
-//         product_id: productId,
-//       },
-//     });
+      // Add picture to DB
+      await Picture.create({
+        product_id: productId,
+        name: imageName,
+        url: publicURL,
+      });
+    }
+  } catch (error) {
+    throw error;
+  }
+};
 
-//     // Remove existing pictures
-//     for (const picture of pictures) {
-//       const imagePath = `images/${picture.name}`;
+const updateProductImages = async (images, productId) => {
+  if (!images || images.length < 1) return;
 
-//       await admin
-//         .storage()
-//         .bucket()
-//         .file(imagePath)
-//         .delete({ ignoreNotFound: true });
-//     }
-//     // Remove picture from DB
-//     await Picture.destroy({ where: { product_id: productId } });
-//   } catch (error) {
-//     throw error;
-//   }
-// };
+  try {
+    await deleteProductImages(productId);
 
-// const uploadProfileImage = async (image, userId) => {
-//   if (!image) return;
+    await uploadProductImages(images, productId);
+  } catch (error) {
+    throw error;
+  }
+};
 
-//   try {
-//     // Remove existing profile picture
-//     await deleteProfileImage(userId);
+const deleteProductImages = async (productId) => {
+  try {
+    // Get existing pictures
+    const pictures = await Picture.findAll({
+      where: {
+        product_id: productId,
+      },
+    });
 
-//     const newPictureName = v4();
+    // Remove existing pictures
+    for (const picture of pictures) {
+      const imagePath = `images/${picture.name}`;
 
-//     const imageExt = image.mimetype.replace("image/", "");
-//     const imageName = `${newPictureName}.${imageExt}`;
-//     const imagePath = `profiles/${imageName}`;
+      // await admin
+      //   .storage()
+      //   .bucket()
+      //   .file(imagePath)
+      //   .delete({ ignoreNotFound: true });
+        
+      await supabase.storage.from('bucket').remove(imagePath)
+    }
 
-//     // Upload picture
-//     await admin.storage().bucket().file(imagePath).save(image.buffer);
 
-//     await admin.storage().bucket().file(imagePath).makePublic();
+    // Remove picture from DB
+    await Picture.destroy({ where: { product_id: productId } });
+  } catch (error) {
+    throw error;
+  }
+};
 
-//     const publicUrl = admin.storage().bucket().file(imagePath).publicUrl();
+const uploadProfileImage = async (image, userId) => {
+  if (!image) return;
 
-//     // Add picture to DB
-//     await UserBiodata.update(
-//       {
-//         picture: publicUrl,
-//       },
-//       {
-//         where: {
-//           user_id: userId,
-//         },
-//       }
-//     );
-//   } catch (error) {
-//     throw error;
-//   }
-// };
+  try {
+    // Remove existing profile picture
+    await deleteProfileImage(userId);
 
-// const deleteProfileImage = async (userId) => {
-//   try {
-//     const userBio = await UserBiodata.findOne({
-//       where: {
-//         user_id: userId,
-//       },
-//     });
+    const newPictureName = v4();
 
-//     if (!userBio || !userBio.picture) return;
+    const imageExt = image.mimetype.replace("image/", "");
+    const imageName = `${newPictureName}.${imageExt}`;
+    const imagePath = `profiles/${imageName}`;
 
-//     const pictureName = userBio.picture
-//       .replace(
-//         "https://storage.googleapis.com/final-project-binar-d4a7b.appspot.com/profiles%2F",
-//         ""
-//       )
-//       .trim();
+    // Upload picture
+    // await admin.storage().bucket().file(imagePath).save(image.buffer);
 
-//     const imagePath = `profiles/${pictureName}`;
+    // await admin.storage().bucket().file(imagePath).makePublic();
 
-//     await admin
-//       .storage()
-//       .bucket()
-//       .file(imagePath)
-//       .delete({ ignoreNotFound: true });
+    // const publicUrl = admin.storage().bucket().file(imagePath).publicUrl();
 
-//     await UserBiodata.update(
-//       {
-//         picture: null,
-//       },
-//       {
-//         where: {
-//           user_id: userId,
-//         },
-//       }
-//     );
-//   } catch (error) {
-//     throw error;
-//   }
-// };
+    const { data, error } = await supabase.storage.from('secondhand').upload(imagePath, file);
+    console.log(data)
 
-// module.exports = {
-//   validatePicture,
-//   uploadProductImages,
-//   updateProductImages,
-//   deleteProductImages,
-//   uploadProfileImage,
-// };
+    const { publicURL, error: urlError } = supabase.storage.from('secondhand').getPublicUrl(imagePath);
+    console.log(publicURL)
+
+    // Add picture to DB
+    await UserBiodata.update(
+      {
+        picture: publicURL,
+      },
+      {
+        where: {
+          user_id: userId,
+        },
+      }
+    );
+  } catch (error) {
+    throw error;
+  }
+};
+
+const deleteProfileImage = async (userId) => {
+  try {
+    const userBio = await UserBiodata.findOne({
+      where: {
+        user_id: userId,
+      },
+    });
+
+    if (!userBio || !userBio.picture) return;
+
+    const pictureName = userBio.picture
+      .replace(
+        "https://storage.googleapis.com/final-project-binar-d4a7b.appspot.com/profiles%2F",
+        ""
+      )
+      .trim();
+
+    const imagePath = `profiles/${pictureName}`;
+
+    // await admin
+    //   .storage()
+    //   .bucket()
+    //   .file(imagePath)
+    //   .delete({ ignoreNotFound: true });
+
+    await supabase.storage.from('bucket').remove(imagePath)
+
+    await UserBiodata.update(
+      {
+        picture: null,
+      },
+      {
+        where: {
+          user_id: userId,
+        },
+      }
+    );
+  } catch (error) {
+    throw error;
+  }
+};
+
+module.exports = {
+  validatePicture,
+  uploadProductImages,
+  updateProductImages,
+  deleteProductImages,
+  uploadProfileImage,
+};
