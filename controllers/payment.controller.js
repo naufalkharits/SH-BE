9b017c3f7c12f040cb9c80871ed07cfa8d637df6
Naufal <80172679+naufalkharits@users.noreply.github.com>
2025-02-28 06:42:03 +0700

    midtrans

--- controllers/payment.controller.js
+++ controllers/payment.controller.js
@@ -1,135 +1,172 @@
 require("dotenv").config()
-const Xendit = require("xendit-node")
+const midtransClient = require('midtrans-client');
 
-const { Transaction, Product } = require("../models")
+// const Xendit = require("xendit-node")
 
-const x = new Xendit({
-  secretKey: `${process.env.PAYMENT_SECRET_API_KEY}`,
-})
-const { Invoice } = x
-const invoiceSpecificOptions = {}
-const i = new Invoice(invoiceSpecificOptions)
+// const { Transaction, Product } = require("../models")
+
+// const x = new Xendit({
+//   secretKey: `${process.env.PAYMENT_SECRET_API_KEY}`,
+// })
+// const { Invoice } = x
+// const invoiceSpecificOptions = {}
+// const i = new Invoice(invoiceSpecificOptions)
 
 module.exports = {
-  getInvoice: async (req, res) => {
+  getSnap: async (req, res) => {
     try {
-      const transaction = await Transaction.findOne({
-        where: { id: req.body.external_id },
+      // Create Snap API instance
+      const snap = new midtransClient.Snap({
+        // Set to true if you want Production Environment (accept real transaction).
+        isProduction : false,
+        serverKey : process.env.MIDTRANS_SERVER_KEY
       })
 
-      if (transaction.status !== "ACCEPTED") {
-        return res.status(404).json({
-          type: "NOT_FOUND",
-          message: "Transaction not found",
-        })
-      }
-
-      const data = {
-        externalID: req.body.external_id,
-        amount: req.body.amount,
-        fees: [
-          {
-            type: "ADMIN",
-            value: 5000,
-          },
-        ],
-        customer: {
-          email: req.body.email,
-          mobile_number: `+${req.body.mobile_number}`,
+      const parameter = {
+        "transaction_details": {
+          "order_id": req.body.order_id,
+          "gross_amount": req.body.amount
         },
-        customer_notification_preference: {
-          invoice_created: ["email"],
-          invoice_reminder: ["email"],
-          invoice_paid: ["email"],
-          invoice_expired: ["email"],
+        "credit_card":{
+          "secure" : true
         },
-        invoiceDuration: 120,
-        successRedirectURL: req.body.redirect_url,
-        failureRedirectURL: req.body.redirect_url,
-      }
-
-      const response = await i.createInvoice(data)
-
-      await Transaction.update(
-        {
-          status: "WAIT FOR PAYMENT",
-          expiry_date: response.expiry_date,
-          invoice_id: response.id,
-          invoice_url: response.invoice_url,
-        },
-        {
-          where: {
-            id: response.external_id,
-          },
+        "customer_details": {
+          "first_name": "sandbox_firstname",
+          "last_name": "sandbox_lastname",
+          "email": req.body.email,
+          "phone": `+${req.body.mobile_number}`
         }
-      )
+      }
 
-      return res.status(200).json({ invoice: response })
+      snap.createTransaction(parameter).then((transaction)=>{
+        // transaction token
+        let transactionToken = transaction.token;
+        console.log('transactionToken:',transactionToken);
+      })    
     } catch (error) {
       return res.status(500).json({ type: "SYSTEM_ERROR", message: "Something wrong with server" })
     }
-  },
-  webhookInvoice: async (req, res) => {
-    try {
-      const transaction = await Transaction.findOne({
-        where: { id: req.body.external_id },
-      })
 
-      if (req.body.status === "PAID") {
-        await Transaction.update(
-          {
-            status: "PAID",
-          },
-          {
-            where: {
-              id: req.body.external_id,
-            },
-          }
-        )
+//   getInvoice: async (req, res) => {
+//     try {
+//       const transaction = await Transaction.findOne({
+//         where: { id: req.body.external_id },
+//       })
 
-        await Product.update(
-          {
-            status: "SOLD",
-          },
-          {
-            where: {
-              id: transaction.product_id,
-            },
-          }
-        )
-      }
+//       if (transaction.status !== "ACCEPTED") {
+//         return res.status(404).json({
+//           type: "NOT_FOUND",
+//           message: "Transaction not found",
+//         })
+//       }
 
-      if (req.body.status === "EXPIRED") {
-        // const response = await i.expireInvoice({
-        //   invoiceID: req.body.id,
-        // });
+//       const data = {
+//         externalID: req.body.external_id,
+//         amount: req.body.amount,
+//         fees: [
+//           {
+//             type: "ADMIN",
+//             value: 5000,
+//           },
+//         ],
+//         customer: {
+//           email: req.body.email,
+//           mobile_number: `+${req.body.mobile_number}`,
+//         },
+//         customer_notification_preference: {
+//           invoice_created: ["email"],
+//           invoice_reminder: ["email"],
+//           invoice_paid: ["email"],
+//           invoice_expired: ["email"],
+//         },
+//         invoiceDuration: 120,
+//         successRedirectURL: req.body.redirect_url,
+//         failureRedirectURL: req.body.redirect_url,
+//       }
 
-        await Transaction.update(
-          {
-            status: "REJECTED",
-          },
-          {
-            where: {
-              id: req.body.external_id,
-            },
-          }
-        )
+//       const response = await i.createInvoice(data)
 
-        await Product.update(
-          {
-            status: "READY",
-          },
-          {
-            where: {
-              id: transaction.product_id,
-            },
-          }
-        )
-      }
+//       await Transaction.update(
+//         {
+//           status: "WAIT FOR PAYMENT",
+//           expiry_date: response.expiry_date,
+//           invoice_id: response.id,
+//           invoice_url: response.invoice_url,
+//         },
+//         {
+//           where: {
+//             id: response.external_id,
+//           },
+//         }
+//       )
 
-      return res.status(200)
-    } catch (error) {
-      return res.status(500)
-    }
-  },
+//       return res.status(200).json({ invoice: response })
+//     } catch (error) {
+//       return res.status(500).json({ type: "SYSTEM_ERROR", message: "Something wrong with server" })
+//     }
+//   },
+//   webhookInvoice: async (req, res) => {
+//     try {
+//       const transaction = await Transaction.findOne({
+//         where: { id: req.body.external_id },
+//       })
+
+//       if (req.body.status === "PAID") {
+//         await Transaction.update(
+//           {
+//             status: "PAID",
+//           },
+//           {
+//             where: {
+//               id: req.body.external_id,
+//             },
+//           }
+//         )
+
+//         await Product.update(
+//           {
+//             status: "SOLD",
+//           },
+//           {
+//             where: {
+//               id: transaction.product_id,
+//             },
+//           }
+//         )
+//       }
+
+//       if (req.body.status === "EXPIRED") {
+//         // const response = await i.expireInvoice({
+//         //   invoiceID: req.body.id,
+//         // });
+
+//         await Transaction.update(
+//           {
+//             status: "REJECTED",
+//           },
+//           {
+//             where: {
+//               id: req.body.external_id,
+//             },
+//           }
+//         )
+
+//         await Product.update(
+//           {
+//             status: "READY",
+//           },
+//           {
+//             where: {
+//               id: transaction.product_id,
+//             },
+//           }
+//         )
+//       }
+
+//       return res.status(200)
+//     } catch (error) {
+//       return res.status(500)
+//     }
+//   },
+  }
 }
--- routes/payment.route.js
+++ routes/payment.route.js
@@ -1,7 +1,8 @@
 const router = require("express").Router()
 const paymentController = require("../controllers/payment.controller")
 
-router.post("/invoice", paymentController.getInvoice)
-router.post("/invoice/webhook", paymentController.webhookInvoice)
+router.post("/snap", paymentController.getSnap)
+// router.post("/invoice", paymentController.getInvoice)
+// router.post("/invoice/webhook", paymentController.webhookInvoice)
 
 module.exports = router
